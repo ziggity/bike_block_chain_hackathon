f06c846bc932bdb17767fb903c0bd237af2f9b65
zach beecher <zachbeecher@gmail.com>
2018-07-21 14:33:16 -0700

    api routes, profile education, experience added delete option

--- validation/register.js
+++ validation/register.js
@@ -7,7 +7,7 @@ module.exports = function validateRegisterInput(data) {
   data.name = !isEmpty(data.name) ? data.name : '';
   data.email = !isEmpty(data.email) ? data.email : '';
   data.password = !isEmpty(data.password) ? data.password : '';
-  //data.password2 = !isEmpty(data.password2) ? data.password2 : '';
+  data.password2 = !isEmpty(data.password2) ? data.password2 : '';
 
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = 'Name must be between 2 and 30 characters';
@@ -33,13 +33,13 @@ module.exports = function validateRegisterInput(data) {
     errors.password = 'Password must be at least 6 characters';
   }
 
-//   if (Validator.isEmpty(data.password2)) {
-//     errors.password2 = 'Confirm Password field is required';
-//   }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = 'Confirm Password field is required';
+  }
 
-//   if (!Validator.equals(data.password, data.password2)) {
-//     errors.password2 = 'Passwords must match';
-//   }
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = 'Passwords must match';
+  }
 
   return {
     errors,
