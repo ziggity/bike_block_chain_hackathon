454adf359c4ffd34d951a9ec49b30d08dd164efe
zach beecher <zachbeecher@gmail.com>
2018-07-21 13:32:09 -0700

    mongodb user authentication with jwtokens

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://zach:zach11@ds145911.mlab.com:45911/bike_blockchain_hackathon'
+    mongoURI: 'mongodb://zach:zach11@ds145911.mlab.com:45911/bike_blockchain_hackathon',
+    secretOrKey: 'secret'
 }
