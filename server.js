f9645920fac33583bc95a534d2fcb35f5b81f049
zach beecher <zachbeecher@gmail.com>
2018-07-21 12:17:15 -0700

    add users router with gravatar and salt password functionality

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
