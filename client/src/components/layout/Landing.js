0ad741e40a4a90e5408c32155f5ddd595c03bdfc
zach beecher <zachbeecher@gmail.com>
2018-07-21 18:01:11 -0700

    fix missing issues

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,7 +17,7 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Connector</h1>
+                <h1 className="display-3 mb-4">BikeBlockChain Connector</h1>
                 <p className="lead">
                   {' '}
                   Create a bikeBlockChain profile, share posts and get help
